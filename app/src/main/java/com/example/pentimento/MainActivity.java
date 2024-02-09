4dac35f4924fd1381866a2a92a02fdd6a8ba0f31
MoranBehar <moran.behar@gmail.com>
2024-02-09 13:44:31 +0200

    set main page

--- app/src/main/java/com/example/pentimento/MainActivity.java
+++ app/src/main/java/com/example/pentimento/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.pentimento;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    
+    ImageView ivPhoto;
+    TextView tvMsg;
+    Button btnAddMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        findViews();
+        setListeners();
+    }
+
+    private void findViews() {
+        ivPhoto = findViewById(R.id.ivPhoto);
+        tvMsg = findViewById(R.id.tvMsg);
+        btnAddMsg = findViewById(R.id.btnAddMsg);
+    }
+
+    private void setListeners() {
+        btnAddMsg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnAddMsg)
+        {
+            addSecretMsgToPhoto();
+        }
+    }
+
+    private void addSecretMsgToPhoto() {
     }
 }
