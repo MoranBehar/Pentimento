85efb839e1f5045087df1276b9121805fec1892e
MoranBehar <moran.behar@gmail.com>
2024-02-22 16:25:15 +0200

    log in page coding + fragments activity xml file

--- app/src/main/java/com/example/pentimento/LogInActivity.java
+++ app/src/main/java/com/example/pentimento/LogInActivity.java
@@ -1,21 +1,90 @@
 package com.example.pentimento;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LogInActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText etLogInEmail, etLogInPassword;
     Button btnLogIn, btnNeedSignup;
+
+    FirebaseAuth fbAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        findViews();
+        setListeners();
+        fbAuth = FirebaseAuth.getInstance();
+    }
 
+    private void findViews() {
+        etLogInEmail = findViewById(R.id.etLogInEmail);
+        etLogInPassword = findViewById(R.id.etLogInPassword);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnNeedSignup = findViewById(R.id.btnNeedSignup);
+    }
+
+    private void setListeners() {
+        btnLogIn.setOnClickListener(this);
+        btnNeedSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnLogIn)
+        {
+            logInUserToTheApp();
+        }
+        else if(v == btnNeedSignup)
+        {
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void logInUserToTheApp() {
+        String email = etLogInEmail.getText().toString();
+        String password = etLogInPassword.getText().toString();
+
+        fbAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Intent intent = new Intent(LogInActivity.this,
+                                    FragmentsActivity.class);
+                            startActivity(intent);
+                        }
+                        else if(email != fbAuth.getCurrentUser().getEmail())
+                        {
+                            Toast.makeText(LogInActivity.this,
+                                    "Email incorrect",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(LogInActivity.this,
+                                    "Password incorrect",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
