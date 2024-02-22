40bd411e5c8b7ae2d5135f4da4fc580cb90a13bf
MoranBehar <moran.behar@gmail.com>
2024-02-22 19:58:35 +0200

    signup + user class

--- app/src/main/java/com/example/pentimento/SignUpActivity.java
+++ app/src/main/java/com/example/pentimento/SignUpActivity.java
@@ -1,15 +1,26 @@
 package com.example.pentimento;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -48,7 +59,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         if(v == btnSignUp)
         {
-//            signUpUserToApp();
+            startSignUpProses();
         }
         else if(v == btnNeedLogin)
         {
@@ -57,11 +68,132 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-//    private void signUpUserToApp() {
-//        if(isEmailOK(email) && isPasswordOK(password) && isNameOK(name) &&
-//                isAgeOK(age) && isPhoneOK(phone)){
-//
-//        }
-//
-//    }
+    private void startSignUpProses(){
+        String name = etSignUpName.getText().toString();
+        String email = etSignUpEmail.getText().toString();
+        String phone = etSignUpPhoneNumber.getText().toString();
+        String age = etSignUpAge.getText().toString();
+        String password = etSignUpPassword.getText().toString();
+
+        if(isEmailOK(email) && isPasswordOK(password) && isNameOK(name) &&
+                isAgeOK(age) && isPhoneOK(phone)) {
+
+            signUpUserToApp(name, email, phone, password, age);
+        }
+        else if(!isEmailOK(email))
+        {
+            Toast.makeText(SignUpActivity.this, "Email not valid",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if(!isAgeOK(age))
+        {
+            Toast.makeText(SignUpActivity.this, "Age not valid",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if(!isNameOK(name))
+        {
+            Toast.makeText(SignUpActivity.this, "Name not valid",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if(!isPasswordOK(password))
+        {
+            Toast.makeText(SignUpActivity.this, "Password not valid",
+                    Toast.LENGTH_LONG).show();
+        }
+        else if(!isPhoneOK(phone))
+        {
+            Toast.makeText(SignUpActivity.this, "Phone not valid",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void signUpUserToApp(String name, String email, String phone,
+                                 String password, String age) {
+
+        fbAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignUpActivity.this,
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    FirebaseFirestore store = FirebaseFirestore.getInstance();
+                                    User user = new User(email, name, phone, Integer.parseInt(age));
+
+                                    store.collection("users").document(fbAuth.getUid())
+                                            .set(user)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    Intent intent = new Intent(SignUpActivity.this,
+                                                            FragmentsActivity.class);
+                                                    startActivity(intent);
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(SignUpActivity.this,
+                                                            "Failed to register",
+                                                            Toast.LENGTH_LONG).show();
+                                                }
+                                            });
+
+                                    Toast.makeText(SignUpActivity.this,
+                                            "Wait...",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(SignUpActivity.this,
+                                            "Failed to register",
+                                            Toast.LENGTH_LONG).show();
+                                    Log.d("error", task.getException().toString());
+                                }
+                            }
+                        });
+    }
+
+    private boolean isEmailOK(String email) {
+        if (android.text.TextUtils.isEmpty(email))
+            return false;
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            return false;
+
+        return true;
+    }
+    private boolean isPasswordOK(String password) {
+        if (android.text.TextUtils.isEmpty(password))
+            return false;
+        else if (password.length() < 6)
+            return false;
+
+        return true;
+    }
+    private boolean isNameOK(String name) {
+        if (android.text.TextUtils.isEmpty(name))
+            return false;
+        else if (name.length() < 2)
+            return false;
+        else if(!name.chars().allMatch(Character::isLetter))
+            return false;
+
+        return true;
+    }
+    private boolean isAgeOK(String age) {
+        int ageNum=Integer.parseInt(age);
+        if (android.text.TextUtils.isEmpty(age))
+            return false;
+        else if (ageNum < 15)
+            return false;
+
+        return true;
+    }
+    private boolean isPhoneOK(String phone) {
+        if (TextUtils.isEmpty(phone))
+            return false;
+        else if (!phone.substring(0, 2).equals("05") || phone.charAt(3)!='-')
+            return false;
+
+        return true;
+    }
 }
