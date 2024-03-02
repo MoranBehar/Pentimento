d3e51fddea868ca30224df2d529dac83d2b24bd0
MoranBehar <moran.behar@gmail.com>
2024-03-02 23:29:53 +0200

    photo view - WIP

--- app/src/main/java/com/example/pentimento/PhotoActivityMenusClass.java
+++ app/src/main/java/com/example/pentimento/PhotoActivityMenusClass.java
@@ -1,11 +1,15 @@
 package com.example.pentimento;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public abstract class PhotoActivityMenusClass extends AppCompatActivity {
+
+    ImageButton btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,20 @@ public abstract class PhotoActivityMenusClass extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.photoToolbar);
         setSupportActionBar(toolbar);
 
-
-
+        backToGallery();
     }
 
     protected abstract int getLayoutId();
 
+
+    //TODO - doesn't work
+    private void backToGallery(){
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
